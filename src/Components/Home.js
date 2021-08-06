19f885b526b76dc5305046a209fcf07d350a1057
BhupathiVenkataSaiCharan <77479689+BhupathiVenkataSaiCharan@users.noreply.github.com>
2021-08-06 18:45:25 +0530

    Web App

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React, {Component, useState, useEffect} from 'react';
+import Card1 from "../Cards/Card1";
+
+class Home extends Component{
+    render(){
+        return(
+            <>
+            <Card1/>
+            </>
+        );
+    }
+}
+
+export default Home;
